2beced1fb72429109d3d52c64bd65ec80be20eb1
thirstyice <thirstyice@users.noreply.github.com>
2026-01-27 19:41:15 -0700

    Simplify Settings

---
+++ include/macros.h
@@ -0,0 +1,48 @@
+/*******************************************************************************
+* Project: smARTbulb                                                           *
+* Filename: /include/macros.h                                                  *
+*                                                                              *
+* Created: 2026-01-27                                                          *
+* Author: thirstyice                                                           *
+*                                                                              *
+* Copyright (c) 2026 Tauran - https://github.com/thirstyice                    *
+* For details see smARTbulb/LICENSE (if applicable)                            *
+*                                                                              *
+*******************************************************************************/
+
+/**
+** @file
+** @brief Assorted handy macros
+**
+**/
+
+
+#define PARENS ()
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+
+/**
+** @brief Apply a macro to each of a variable number of args
+** @param macro: The macro to apply
+**/
+#define FOR_EACH(macro, ...)                                    \
+  __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...)                         \
+  macro(a1)                                                     \
+  __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+
+/**
+** @brief Transforms the argument into a string
+**/
+#define STRINGIFY(x) #x
+
+#define _STR_ARRAY(x) , STRINGIFY(x)
+/**
+** @brief Transforms the arguments into a list of strings,
+** e.g. for array initialization
+**/
+#define STR_ARRAY(x, ...) STRINGIFY(x) __VA_OPT__(FOR_EACH(_STR_ARRAY, __VA_ARGS__))
--- src/Settings.h
+++ src/Settings.h
@@ -12,151 +12,16 @@
 #pragma once
 
 #include <Arduino.h>
-
-#if defined(ESP32)
 #include <Preferences.h>
-class SettingsManager {
-private:
-static Preferences prefs;
-Preferences* getPrefs(const char*, bool);
-public:
-	size_t put(const char *key, int8_t value);
-	size_t put(const char *key, uint8_t value);
-	size_t put(const char *key, int16_t value);
-	size_t put(const char *key, uint16_t value);
-	size_t put(const char *key, int32_t value);
-	size_t put(const char *key, uint32_t value);
-	size_t put(const char *key, int32_t value);
-	size_t put(const char *key, uint32_t value);
-	size_t put(const char *key, int64_t value);
-	size_t put(const char *key, uint64_t value);
-	size_t put(const char *key, float_t value);
-	size_t put(const char *key, double_t value);
-	size_t put(const char *key, bool value);
-	size_t put(const char *key, String value);
+#include "macros.h"
 
-	size_t putBytes(const char *key, const void *value, size_t len);
+#define Settings(section, ...) namespace Settings { \
+enum section : unsigned char { __VA_ARGS__, End }; \
+const char* const section[section::End] = { STR_ARRAY(__VA_ARGS__)}; \
+}
 
-	int8_t get(const char *key, int8_t defaultValue = 0);
-	uint8_t get(const char *key, uint8_t defaultValue = 0);
-	int16_t get(const char *key, int16_t defaultValue = 0);
-	uint16_t get(const char *key, uint16_t defaultValue = 0);
-	int32_t get(const char *key, int32_t defaultValue = 0);
-	uint32_t get(const char *key, uint32_t defaultValue = 0);
-	int32_t get(const char *key, int32_t defaultValue = 0);
-	uint32_t get(const char *key, uint32_t defaultValue = 0);
-	int64_t get(const char *key, int64_t defaultValue = 0);
-	uint64_t get(const char *key, uint64_t defaultValue = 0);
-	float_t get(const char *key, float_t defaultValue = NAN);
-	double_t get(const char *key, double_t defaultValue = NAN);
-	bool get(const char *key, bool defaultValue = false);
-	String get(const char *key, String defaultValue = String());
+Preferences* getPrefs(const char* section);
 
-	size_t getBytesLength(const char *key);
-	size_t getBytes(const char *key, void *buf, size_t maxLen);
-};
-
-#elif defined(ESP8266)
-class SettingsManager {
-	private:
-
-	public:
-		size_t put(const char *key, int8_t value);
-		size_t put(const char *key, uint8_t value);
-		size_t put(const char *key, int16_t value);
-		size_t put(const char *key, uint16_t value);
-		size_t put(const char *key, int32_t value);
-		size_t put(const char *key, uint32_t value);
-		size_t put(const char *key, int32_t value);
-		size_t put(const char *key, uint32_t value);
-		size_t put(const char *key, int64_t value);
-		size_t put(const char *key, uint64_t value);
-		size_t put(const char *key, float_t value);
-		size_t put(const char *key, double_t value);
-		size_t put(const char *key, bool value);
-		size_t put(const char *key, String value);
-
-		size_t putBytes(const char *key, const void *value, size_t len);
-
-		int8_t get(const char *key, int8_t defaultValue = 0);
-		uint8_t get(const char *key, uint8_t defaultValue = 0);
-		int16_t get(const char *key, int16_t defaultValue = 0);
-		uint16_t get(const char *key, uint16_t defaultValue = 0);
-		int32_t get(const char *key, int32_t defaultValue = 0);
-		uint32_t get(const char *key, uint32_t defaultValue = 0);
-		int32_t get(const char *key, int32_t defaultValue = 0);
-		uint32_t get(const char *key, uint32_t defaultValue = 0);
-		int64_t get(const char *key, int64_t defaultValue = 0);
-		uint64_t get(const char *key, uint64_t defaultValue = 0);
-		float_t get(const char *key, float_t defaultValue = NAN);
-		double_t get(const char *key, double_t defaultValue = NAN);
-		bool get(const char *key, bool defaultValue = false);
-		String get(const char *key, String defaultValue = String());
-
-		size_t getBytesLength(const char *key);
-		size_t getBytes(const char *key, void *buf, size_t maxLen);
-	};
-
-#endif /* ESP32 */
-
-namespace Settings {
-
-SettingsManager manager;
-
-struct Network { enum : unsigned char {
-	Ip,
-	Subnet,
-	Gateway,
-	Ssid1,
-	Pass1,
-	Ssid2,
-	Pass2,
-	Hostname,
-	End
-};
-const char* key[End] = {
-	"netip",
-	"netsubnet",
-	"netgateway",
-	"netssid1",
-	"netpass1",
-	"netssid2",
-	"netpass2",
-	"nethost"
-};
-};
-struct Light { enum : unsigned char {
-	Type,
-	Gpio,
-	ChanMap,
-	StartColor,
-	ConnectColor,
-	NoConnectColor,
-	End
-};
-const char* key[End] = {
-	"lighttype",
-	"lightgpio",
-	"lightmap",
-	"lightstart",
-	"lightconn",
-	"lightnoconn"
-};
-};
-struct Control { enum : unsigned char {
-	Protocol,
-	Net,
-	Subnet,
-	Universe,
-	Address,
-	End
-};
-const char* key[End] = {
-	"conproto",
-	"connet",
-	"consubnet",
-	"conuniverse",
-	"conaddress"
-};
-};
-} // namespace Settings
+Settings(Network, ip, subnet, gateway, ssid1, pass1, ssid2, pass2, hostname);
+Settings(Light, type, gpio, chanmap, startcolor, connectcolor, noconnectcolor);
+Settings(Control, protocol, net, subnet, universe, address);
