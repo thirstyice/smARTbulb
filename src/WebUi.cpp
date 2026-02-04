d8871f9975bc2910255b8ffe737affae6a9eb9a5
thirstyice <thirstyice@users.noreply.github.com>
2026-02-04 05:34:55 -0700

    Fix template bypass for non-html files

--- src/WebUi.cpp
+++ src/WebUi.cpp
@@ -23,39 +23,40 @@ AsyncWebSocketMessageHandler wsHandler;
 AsyncWebSocket ws("/ws", wsHandler.eventHandler());
 
 void begin() {
-	server.on("*.html", HTTP_GET, [](AsyncWebServerRequest *request) {
-		request->send(LittleFS, "/webui" + request->url(), "text/html", false, [=](const String &var) -> String {
-			if (var == "HOSTNAME") {
-				return WiFi.getHostname();
-			}
-			if (var == "VERSION") {
-				return SMARTBULB_VERSION_STR;
-			}
-			if (request->url().startsWith("/config")) {
-				if (var == "IP") {
-					return Networking::ip.toString();
-				}
-				if (var == "SSID1") {
-					return Networking::wifiSSID1;
-				}
-				if (var == "SSID2") {
-					return Networking::wifiSSID2;
-				}
-				if (var == "SUBNET") {
-					return Networking::subnet.toString();
-				}
-				if (var == "GATEWAY") {
-					return Networking::gateway.toString();
-				}
-			}
-			if (var == "IP") {
-				return WiFi.localIP().toString();
-			}
-			return emptyString;
-		});
-	});
 	server.on("", HTTP_GET, [](AsyncWebServerRequest *request) {
-		request->send(LittleFS, "/webui" + request->url());
+		if (request->url().endsWith(".html") || request->url().endsWith(".htm")) {
+			request->send(LittleFS, "/webui" + request->url(), "text/html", false, [=](const String &var) -> String {
+				if (var == "HOSTNAME") {
+					return WiFi.getHostname();
+				}
+				if (var == "VERSION") {
+					return SMARTBULB_VERSION_STR;
+				}
+				if (request->url().startsWith("/config")) {
+					if (var == "IP") {
+						return Networking::ip.toString();
+					}
+					if (var == "SSID1") {
+						return Networking::wifiSSID1;
+					}
+					if (var == "SSID2") {
+						return Networking::wifiSSID2;
+					}
+					if (var == "SUBNET") {
+						return Networking::subnet.toString();
+					}
+					if (var == "GATEWAY") {
+						return Networking::gateway.toString();
+					}
+				}
+				if (var == "IP") {
+					return WiFi.localIP().toString();
+				}
+				return emptyString;
+			});
+		} else {
+			request->send(LittleFS, "/webui" + request->url());
+		}
 	});
 	server.rewrite("/", "/index.html");
 
